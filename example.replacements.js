debdabe22fcd32557bfab25eacbcf4b60b156e67
phette23 <phette23@gmail.com>
2017-04-06 16:10:56 -0700

    add in Division Admin UUIDs for reference

--- example.replacements.js
+++ example.replacements.js
@@ -7,6 +7,11 @@ module.exports = {
 
     // program/division role UUIDs
     'admin-role-uuid': 'f3882184-5282-44bd-a7f6-72192a0e46da',
+    // Division Administrator role UUIDs:
+    // - Design: b877dea9-2b12-47da-8771-64a1054b51f9
+    // - Fine Arts: 0aab4094-c44f-4592-b1e1-9c1a163e65b5
+    // - Architecture: 51fbe0ca-7b6c-4c2e-bb79-c01624b1b730
+    // - Humanities & Sciences: 7af528b4-4555-427c-97b9-74b572b00975
     'division-admin-role-uuid': 'f3882184-44bd-44bd-a7f6-72192a0e46da',
     'external-reviewer-role-uuid': '72192a0e-5282-5282-a7f6-f3882184',
     // can be left null if no work study role exists
