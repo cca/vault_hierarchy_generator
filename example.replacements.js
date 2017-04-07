43c77b1d7b22c8bd35cfdfcbfe3f3b97fd58995e
phette23 <phette23@gmail.com>
2017-04-07 14:19:23 -0700

    reorganize example.replacements.js

--- example.replacements.js
+++ example.replacements.js
@@ -1,21 +1,21 @@
 module.exports = {
     // this is always the same
-    'college-admin-uuid': '98058527-dcc9-4a65-8f11-f844d335ffaf',
+    'college-admin-id': '98058527-dcc9-4a65-8f11-f844d335ffaf',
     // external review metadata
-    'collection-uuid': '8faafb5c-7617-4c17-80f7-b9db9b0fd06f',
+    'collection-id': '8faafb5c-7617-4c17-80f7-b9db9b0fd06f',
     'program': 'Jewelry / Metal Arts',
     // of form: (Spring|Summer|Fall) YYYY
     'semester': 'Spring 2017',
 
     // program/division role UUIDs
-    'admin-role-uuid': 'f3882184-5282-44bd-a7f6-72192a0e46da',
     // Division Administrator role UUIDs:
     // - Design: b877dea9-2b12-47da-8771-64a1054b51f9
     // - Fine Arts: 0aab4094-c44f-4592-b1e1-9c1a163e65b5
     // - Architecture: 51fbe0ca-7b6c-4c2e-bb79-c01624b1b730
     // - Humanities & Sciences: 7af528b4-4555-427c-97b9-74b572b00975
-    'division-admin-role-uuid': 'f3882184-44bd-44bd-a7f6-72192a0e46da',
-    'external-reviewer-role-uuid': '72192a0e-5282-5282-a7f6-f3882184',
+    'division-admin-id': 'f3882184-44bd-44bd-a7f6-72192a0e46da',
+    'program-admin-id': 'f3882184-5282-44bd-a7f6-72192a0e46da',
+    'external-reviewer-id': '72192a0e-5282-5282-a7f6-f3882184',
     // can be left null if no work study role exists
-    'work-study-uuid': '72192a0e-5282-44bd-5282-72192a0e46da',
+    'work-study-id': null,
 }
