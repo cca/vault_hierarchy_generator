62b3bac459087bedb4575dbdf72018402b2023d4
phette23 <phette23@gmail.com>
2019-11-04 17:36:08 -0800

    fix the way ACLs are built
    no separating 'OR' between 1st & 2nd entries

--- index.js
+++ index.js
@@ -19,24 +19,31 @@ const replaceStream = require('replaceStream')
 // load replacement strings
 const replacements = require(path.join(__dirname, opts.r || opts.replacements))
 
+// add all the non-empty role IDs to a single access control list
+// ACLs look like "R:{UUID} R:{UUID} OR R:{UUID} OR" so note that they a) do not
+// have a separating "OR" between 2nd & 3rd entries, b) end in an orphan "OR"
+let buildACL = (replacements) => {
+    let acl = [
+    'program-admin-id',
+    'college-admin-id',
+    'division-admin-id',
+    'external-reviewer-id',
+    'work-study-id',
+        ].reduce((acl, role, idx) => {
+            if (!!replacements[role]) {
+                acl += `R:${replacements[role]} `
+                if (idx > 0) acl += 'OR '
+            }
+            return acl
+        }, '')
+    return acl
+}
+
 fs.createReadStream(opts.t || opts.template)
     // perform replacements
     .pipe(replaceStream('TPL_COLLECTION_UUID', replacements['collection-id']))
     .pipe(replaceStream('TPL_PROGRAM', replacements.program))
     .pipe(replaceStream('TPL_SEMESTER', replacements.semester))
     // do all role UUIDs replacements in one fell swoop
-    .pipe(replaceStream('TPL_WHO', (match) => {
-        // add all the non-empty role IDs to a single access control list
-        let acl = [
-        'program-admin-id',
-        'college-admin-id',
-        'division-admin-id',
-        'external-reviewer-id',
-        'work-study-id',
-            ].reduce((acl, role) => {
-                if (!!replacements[role]) return acl + `R:${replacements[role]} OR `
-                return acl
-            }, '')
-        return acl
-    }))
+    .pipe(replaceStream('TPL_WHO', (match) => buildACL(replacements)))
     .pipe(process.stdout)
